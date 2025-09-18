78f1abea78cbf44b50dc0d210461f2d84a68ef83
Ramu9047 <raamukutty2004@gmail.com>
2025-09-18 10:20:03 +0530

    Final Commit

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,14 @@
+
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
 // })
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-
-// export default defineConfig({
-// plugins: [react()],
-// })
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/houseplant-shop/', // 👈 IMPORTANT: match your repo name
 })
